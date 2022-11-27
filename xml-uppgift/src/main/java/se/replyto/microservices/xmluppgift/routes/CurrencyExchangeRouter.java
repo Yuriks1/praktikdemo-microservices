5468d6eddeabe342a35d21f93bed035a6a2f4bba
Yuriks1 <juris.adijans@iths.se>
2022-11-27 18:06:06 +0100

    Reformat code

--- xml-uppgift/src/main/java/se/replyto/microservices/xmluppgift/routes/CurrencyExchangeRouter.java
+++ xml-uppgift/src/main/java/se/replyto/microservices/xmluppgift/routes/CurrencyExchangeRouter.java
@@ -1,18 +1,21 @@
 package se.replyto.microservices.xmluppgift.routes;
 
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.jackson.JacksonDataFormat;
+import org.apache.camel.dataformat.beanio.BeanIODataFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import se.replyto.microservices.xmluppgift.beans.InboundCurrencyExchange;
-import se.replyto.microservices.xmluppgift.beans.OutboundCurrencyExchange;
 
 @Component
 public class CurrencyExchangeRouter extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
+
+        BeanIODataFormat inboundDataFormat = new BeanIODataFormat("files/xml/InboundMessageBeanIOMapping.xml","inboundCurrencyStream");
+
 
         Logger logger = LoggerFactory.getLogger(getClass());
 
@@ -22,8 +25,12 @@ public class CurrencyExchangeRouter extends RouteBuilder {
 
         from("file:files/xml")
                 .routeId("currencyExchangeRouteId")
-                .unmarshal()
-                .jacksonXml(InboundCurrencyExchange.class)
+                .log(LoggingLevel.INFO, "Original body : ${body}")
+                .split(body().tokenize("\n",1,true))
+                .streaming()
+                .unmarshal(inboundDataFormat)
+//                .unmarshal()
+//                .jacksonXml(InboundCurrencyExchange.class)
                 .log("Body: ${body}")
                 .to("activemq:my-activemq-queue");
 
